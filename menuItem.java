dcd56fdcab5eed254a526f049ab79de6853361ed
bhavsingh2017 <bhavkirs@usc.edu>
2019-03-27 21:19:12 -0700

    menuItem class finished

---
+++ menuItem.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class menuItem {
+	private double price = 0.0;
+	private String name = "";
+	private String restaurant = "No Restuarant";
+	private ArrayList<String> deals = new ArrayList<String>();
+	private double rating;
+	private int userInteract;
+	
+	
+	menuItem(String name, double price, String resta){
+		this.name = name;
+		this.price = price;
+		this.restaurant = resta;
+		rating  = 5;
+		userInteract = 1;
+	}
+	
+	
+	public void userRating(int rate){
+		userInteract++;
+		rating = (rating + rate)/userInteract;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String s){
+		this.name = s;
+	}
+	
+	public void setPrice(double d){
+		this.price = d;
+	}
+	
+	public double getPrice(){	
+		return this.price;
+	}
+	
+	public void addDeal(String deal){
+		this.deals.add(deal);
+	}
+	
+	public ArrayList<String> getDeals(){
+		return this.deals;
+	}
+	
+	
+}
